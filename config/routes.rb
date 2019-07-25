619eeb09aa269667bc849aef2f7d8fc5602737f4
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-07-25 12:03:28 +0300

    Add route show

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
     namespace :api do
       namespace :v1 do
-    resources :posts, only: %i[index create update destroy]
+    resources :posts, only: %i[index create update destroy show]
   end
   end
 end
