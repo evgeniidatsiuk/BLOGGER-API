f9f59abea5b9038ced5e1d03fd2e098ef7070f53
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-08-02 11:46:44 +0300

    configure_permitted_parameters

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,13 @@ class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
   include ActionController::HttpAuthentication::Basic::ControllerMethods
   include ::UnifiedJsonRender
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image nickname email password password_confirmation current_password])
+  end
+
 end
