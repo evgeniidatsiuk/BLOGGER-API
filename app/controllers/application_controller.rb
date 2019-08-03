69c0c9f6e5fa4111102cd43167a29dd02bfeacac
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-08-03 23:33:56 +0300

    Add permits to sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::API
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image nickname email password password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: %i[name image nickname email password password_confirmation current_password])
   end
 
