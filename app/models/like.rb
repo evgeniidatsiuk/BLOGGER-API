c46933f3096ba6ea589758b98ed2f3bad1eb5a5f
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-08-28 13:43:58 +0300

    Add callbacks to other models

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,10 @@
 class Like < ApplicationRecord
   belongs_to :object, polymorphic: true
   has_many :notifications, as: :object, dependent: :destroy
+
+  after_create :create_notification
+
+  def create_notification
+    Notification.create(object_type: 'Like', object_id: id, text: 'Like created!', user_id: user.id)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -8,4 +8,10 @@ class Post < ApplicationRecord
   has_many :notifications, as: :object, dependent: :destroy
 
   validates :title, :body, presence: true
+
+  after_create :create_notification
+
+  def create_notification
+    Notification.create(object_type: 'Post', object_id: id, text: 'Post created!', user_id: user.id)
+  end
 end
