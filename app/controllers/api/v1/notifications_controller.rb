ac1b37480e516adde5a0c92571dd68cd3c1ffd1d
evgeniidatsiuk <evgenii.datsiuk@gmail.com>
2019-08-30 22:45:13 +0300

    Add read/unread methods

--- app/controllers/api/v1/notifications_controller.rb
+++ app/controllers/api/v1/notifications_controller.rb
@@ -2,6 +2,7 @@
 
 class Api::V1::NotificationsController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_notification, except: format(w, index)
   
   def index
     @notifications = current_user.notifications
@@ -9,5 +10,22 @@ class Api::V1::NotificationsController < ApplicationController
       success: true,
       notifications: @notifications
     }
+  end
+
+  def read
+    @notification.update(read: true)
+  end
+
+  def unread
+    @notification.update(read: false)
+  end
+
+  private
+
+  def find_notification
+    @notification = Notification.find_by_id(params[:id])
+  rescue StandardError => e
+    @error = e.message
+    render_error(400, nil, @error)
   end
 end
